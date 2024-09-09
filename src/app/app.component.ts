36541e2a13f74707c4e0531b25a8452f24c80375
Rayhanuc <rayhanctg4@gmail.com>
2024-09-10 00:08:21 +0600

    Angular - fullCalender implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'fullcalendar-app';
+  // title = 'fullcalendar-app';
+  calendarOptions: CalendarOptions = {
+    plugins: [dayGridPlugin],
+    initialView: 'dayGridMonth',
+    weekends: false,
+    events: [
+      { title: 'event 1', date: '2024-09-01' },
+      { title: 'event 2', date: '2024-09-02' },
+      { title: 'Reminder 1', date: '2024-09-12' },
+      { title: 'Meeting', date: '2024-10-02' },
+      { title: 'Grower', date: '2024-11-22' }
+    ]
+  };
+
+  toggleWeekends() {
+    this.calendarOptions.weekends = !this.calendarOptions.weekends // toggle the boolean!
+  }
 }
